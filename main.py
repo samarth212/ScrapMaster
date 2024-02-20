d7cafe269f4f22b62d4733ba6dba20b4d0ea5c43
Samarth <samarth212@gmail.com>
2024-02-20 18:44:20 -0500

    added error catching set up

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://samarthkol.com'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    pass
+else:
+    print("Failed to retrieve the webpage. Status code:", response.status_code)
