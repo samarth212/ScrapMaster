aab8e5a200563b760fe858e121ac263bc15142f4
Samarth <samarth212@gmail.com>
2024-02-20 19:11:27 -0500

    added element insertion

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url = input("enter url:")
+
 element = input("enter target element:")
 elemFilter = input("filter by:")
-if elemFilter == "class" or "id":
+if elemFilter == "class" or elemFilter == "id":
     elemName = input("class/id name:")
+    
+
+
 
 response = requests.get(url)
 
@@ -13,6 +17,7 @@ if response.status_code == 200:
     print("success!")
     soup = BeautifulSoup(response.content, 'html.parser')
 
+    
     result = soup.find_all(element)
     print(result)
 
