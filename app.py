136497c29bc2c29c1d79813c84d949c2098520c0
Samarth <samarth212@gmail.com>
2024-02-24 16:46:03 -0500

    linked to index.html page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
