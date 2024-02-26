18a58a8cc6fb76861670b29ec03d8b03a41d7d2b
Samarth <samarth212@gmail.com>
2024-02-25 19:55:28 -0500

    animations

--- static/index.js
+++ static/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
+    $(".desc").waypoint(function(){
+        $(".desc").addClass("animate__animated animate__fadeInLeft")
+    }, {offset: "80%"})
+
+    $(".forms").waypoint(function(){
+        $(".forms").addClass("animate__animated animate__backInUp")
+    }, {offset: "80%"})
+
+    $(".viewer").waypoint(function(){
+        $(".viewer").addClass("animate__animated animate__fadeInRight")
+    }, {offset: "80%"})
 })
